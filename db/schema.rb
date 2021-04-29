b8a2b2f34a0a81a7ad2ddc47ce9ce832f899e001
Yugesh Palvai <yugeshpalvai21@gmail.com>
2021-04-29 12:08:23 +0530

    drops posts database

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_28_081214) do
+ActiveRecord::Schema.define(version: 2021_04_29_063710) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "description"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
 
 end
