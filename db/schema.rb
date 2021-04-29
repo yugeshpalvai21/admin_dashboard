493b7762d02440b26c4dbd474225e1f61b7f0848
Yugesh Palvai <yugeshpalvai21@gmail.com>
2021-04-29 12:35:01 +0530

    generates PostTag model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_29_063710) do
+ActiveRecord::Schema.define(version: 2021_04_29_064305) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "post_tags", force: :cascade do |t|
+    t.string "name"
+    t.bigint "post_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_post_tags_on_post_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "post_tags", "posts"
 end
